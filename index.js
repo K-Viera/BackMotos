968068a4907d4e5e5fb8b86868accefccdf1bbfb
Kevin <kenvi.009@gmail.com>
2021-10-12 18:54:31 -0500

    Express configuration

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ const morgan = require("morgan");
 const app = express();
 cors = require("cors");
 const bodyparser = require("body-parser");
-
-//dbConnection
 require("dotenv").config();
 require("./src/dbConnection");
+
 const port = process.env.PORT || 5000;
+
+app.set("port", port);
+
+app.use(morgan("dev"));
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(cors({ origin: true }));
+
+//rutes
+
+//start server
 app.listen(port, () => {
   console.log("Listen in the port ", port);
 });
