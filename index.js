65cf4ca24e067c2e1843c358af753df6164e21a3
Simmus7 <64326113+Simmus7@users.noreply.github.com>
2021-10-16 11:51:22 -0500

    prueba port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyparser = require("body-parser");
 // require("dotenv").config();
 require("./src/dbConnection");
 
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
 
 app.set("port", port);
 
@@ -22,6 +22,6 @@ app.use("/user", require("./src/routes/userRoutes"));
 app.use("/vehicle", require("./src/routes/vehicleRoute"));
 
 //start server
-app.listen(port, () => {
+/*app.listen(port, () => {
   console.log("Listen in  the port ", port);
-});
+});*/
