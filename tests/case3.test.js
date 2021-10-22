3395ed895ba77b27ace832554bd7eec97ab0c43b
K-Viera <Kenvi.009@gmail.com>
2021-10-22 11:04:46 -0500

    case 3

---
+++ tests/case3.test.js
@@ -0,0 +1,34 @@
+const request = require("supertest");
+const app = require("../index");
+const {
+  deletePersonByDocument,
+} = require("../src/controllers/personController");
+
+Describe("NewPerson-OldVehicle", () => {
+  const data = {
+    document: "12345",
+    name: "nombre1",
+    secondaryName: "nombre2",
+    lastName: "apellido1",
+    plate: "fgh233",
+  };
+
+  it("Try form", (done) => {
+    request(app)
+      .post("/person/sendForm")
+      .send(data)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(400)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  }).timeout(10000);
+
+  it("Delete Person", (done) => {
+    deletePersonByDocument(data.document).then(() => {
+      done();
+    });
+  });
+});
