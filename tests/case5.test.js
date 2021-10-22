7b3004c8106d60d4d9dfb4d6c45e5cb73c11f16a
K-Viera <Kenvi.009@gmail.com>
2021-10-22 11:04:55 -0500

    case 5

---
+++ tests/case5.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const app = require("../index");
+
+Describe("OldPerson-NewVehicle-NoData", () => {
+  const data = {
+    document: "1000",
+    plate: "12345",
+  };
+
+  it("Try form", (done) => {
+    request(app)
+      .post("/person/sendForm")
+      .send(data)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  }).timeout(10000);
+
+  it("Delete Vehicle", () => {
+    deleteVehicleByPlate(data.document).then(() => {
+      done();
+    });
+  });
+});
